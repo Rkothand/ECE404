167fa1b1adab5d7f9cefe005b5c745c1fe0c4846
Rohith Kothandaraman <rkothand@eceprog2.ecn.purdue.edu>
2023-02-02 17:40:04 -0500

    mult_inv changes

--- ECE404HW3/mult_inv.py
+++ ECE404HW3/mult_inv.py
@@ -2,5 +2,51 @@ import sys
 from BitVector import BitVector
 
 
+def MI(num, mod):
+    '''
+    This function uses ordinary integer arithmetic implementation of the
+    Extended Euclid's Algorithm to find the MI of the first-arg integer
+    vis-a-vis the second-arg integer.
+    '''
+    NUM = num; MOD = mod
+    x, x_old = 0, 1
+    y, y_old = 1, 0
+    while mod:
+        q = num // mod
+        num, mod = mod, num % mod
+        x, x_old = x_old - q * x, x
+        y, y_old = y_old - q * y, y
+    if num != 1:
+        print("\nNO MI. However, the GCD of %d and %d is %u\n" % (NUM, MOD, num))
+    else:
+        MI = (x_old + MOD) % MOD
+        print("\nMI of %d modulo %d is: %d\n" % (NUM, MOD, MI))
+
+
+def GCD(a,b):
+    while b:
+        # print(a,"%",b,"=")         #problem 3                                  
+        a,b = b, a%b
+        # print(b)
+    print("\nGCD: %d\n" % a)
+
+
+def bgcd(a,b):
+    if a == b: return a                                         #(A)
+    if a == 0: return b                                         #(B)
+    if b == 0: return a                                         #(C)
+    if (~a & 1):                                                #(D)
+        if (b &1):                                              #(E)
+            return bgcd(a >> 1, b)                              #(F)
+        else:                                                   #(G)
+            return bgcd(a >> 1, b >> 1) << 1                    #(H)
+    if (~b & 1):                                                #(I)
+        return bgcd(a, b >> 1)                                  #(J)
+    if (a > b):                                                 #(K)
+        return bgcd( (a-b) >> 1, b)                             #(L)
+    return bgcd( (b-a) >> 1, a )                                #(M)
+
 if __name__ == '__main__':
-    
+    MI(4,3)
+
+    # GCD(10946, 19838) #problem 3
