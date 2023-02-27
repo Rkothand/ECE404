2b604286d45aa6be9eb07e459a41224a4c27a12b
Rohith Kothandaraman <rohithko@gmail.com>
2023-02-27 18:10:57 -0500

    hw 5 last bit

--- ECE404HW5/ECE404_HW05_sp23/AESimage.py
+++ ECE404HW5/ECE404_HW05_sp23/AESimage.py
@@ -20,9 +20,12 @@ def ctr_aes_image(iv, imagefile = 'image.ppm', out_file='enc_image.ppm', key_fil
 
     image_out = fp.read()
 
-    image_out.write(header1)
-    image_out.write(header2)
-    image_out.write(header3)
+    image_out.write(pp1)
+    image_out.write(pp2)
+    image_out.write(pp3)
+
+
+
 
     with open(key_file, "r") as f:
         key = f.read()
@@ -30,7 +33,7 @@ def ctr_aes_image(iv, imagefile = 'image.ppm', out_file='enc_image.ppm', key_fil
     aes_function = AES.new(key4, AES.MODE_CTR)
 
     bit_image = BitVector(rawbytes=image)
-    segments = bit_image
+    segments = bit_image //16
     list_segment = [bit_image[i*16: i*16+16] for i in range(segments)]
 
     if __name__ == "__main__":
