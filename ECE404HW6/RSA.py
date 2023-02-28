3d0905672ed78f2e27f275416e552f5fa21979cc
Rohith Kothandaraman <rohithko@gmail.com>
2023-02-28 17:52:34 -0500

    hw6 part 1 done

--- ECE404HW6/RSA.py
+++ ECE404HW6/RSA.py
@@ -27,13 +27,48 @@ def MI(num, mod):
         MI = (x_old + MOD) % MOD
         print("\nMI of %d modulo %d is: %d\n" % (NUM, MOD, MI))
 
-def RSAKeyGen(p,q):
+def RSAKeyGen(pin,qin):
+    pfile = open(pin, 'w')
+    qfile = open(qin, 'w')
     generator = PrimeGenerator(bits = 128)
     p = generator.findPrime()
     q = generator.findPrime()
     n = p*q
     totient = (p-1)*(q-1)
     e = 65537
+    bv1 = BitVector(intVal=0, size =128)
+    bv2 = BitVector(intVal=0, size =128)
+
+    while p == q:
+        p = generator.findPrime()
+        ptemp = p-1
+        etemp1 = e
+
+        while etemp1:
+            p1 = etemp1
+            etemp1 = p1 % etemp1
+
+        while not bv1[0] and bv[0] and etemp1 != 1:
+            p = generator.findPrime()
+            ptemp = p-1
+            etemp1 = e
+
+            while etemp1:
+                p1 = etemp1
+                etemp1 = p1 % etemp1
+        
+        q = generator.findPrime()
+        bv2.set_value(intVal = q)
+        qtemp = q-1
+        etemp2 = e
+
+        while etemp2:
+            q1 = etemp2
+            etemp2 = q1 % etemp2
+
+        pfile.write(str(p))
+        qfile.write(str(q))
+
 
 
 
@@ -91,20 +126,32 @@ def RSADecrypt(encryptedfile, p, q, decryptedfile):
 
 
 if __name__ == "__main__":
-    input = sys.argv[2]
 
-    pfile = sys.argv[3]        
-    p = open(pfile, "r")
-    p = p.readline()
-    qfile = sys.argv[4]
-    q = open(qfile, "r")
-    q =q.readline()
-    output = sys.argv[5]
 
     if sys.argv[1] == "-e": #encryption
+        input = sys.argv[2]
+        output = sys.argv[5]
+        pfile = sys.argv[3]  
+        p = open(pfile, "r")
+        p = p.readline()
+        qfile = sys.argv[4]
+        q = open(qfile, "r")
+        q =q.readline()
         RSAEncrypt(input,p, q, output)
     elif sys.argv[1] == "-d": #decryption
+        input = sys.argv[2]
+        output = sys.argv[5]
+        pfile = sys.argv[3]  
+        p = open(pfile, "r")
+        p = p.readline()
+        qfile = sys.argv[4]
+        q = open(qfile, "r")
+        q =q.readline()
         RSADecrypt(input, p, q, output)
     elif sys.argv[1] == "-g": #keygen
+        pfile = sys.argv[2]
+        p = open(pfile, "w")
+        qfile = sys.argv[3]
+        q = open(qfile, "w")
         RSAKeyGen(p,q)
 
